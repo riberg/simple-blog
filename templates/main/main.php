8180319db1f7d38259cffb1d699996e259d2b4fb
riberg <den.radaeff@gmail.com>
2021-10-27 23:23:38 +1000

    Composer install

--- templates/main/main.php
+++ templates/main/main.php
@@ -3,5 +3,8 @@
     <h2><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h2>
     <p><?= $article->getText() ?></p>
     <hr>
+    <?php if ($user !== null && $user->isAdmin()): ?>
+        <a href="http://blog.loc/articles/add">Создать</a>
+    <?php endif; ?>
 <?php endforeach; ?>
 <?php include __DIR__ . '/../footer.php'; ?>
